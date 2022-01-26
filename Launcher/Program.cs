be70bd208568f6deb40aa7f661da6305b9140230
CeSun <ce.sun@outlook.com>
2022-01-26 21:51:49 +0800

    测试mesh

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -2,17 +2,35 @@
 
 using Launcher.Platform;
 using LiteEngine.Core;
+using OpenTK.Mathematics;
 
-
-var model = new Model();
 Window window = new Window();
 window.Load += () =>
 {
-    model.LoadModel(@"./tr_leet/leet.FBX");
+   var model = new Model();
+    model.LoadModel(@"tr_leet\leet.FBX");
     model.Parent = Scene.Current.Root;
     var f = Camera.Current;
-    f.LocalPosition = new OpenTK.Mathematics.Vector3 {X= 0, Y = 0, Z = 1 };
-    f.LocalRotation = OpenTK.Mathematics.Quaternion.FromEulerAngles(0, (float)Math.PI, 0);
+    f.LocalPosition = new Vector3 {X= 0, Y = 20, Z = 20 };
+    f.LocalRotation = Quaternion.FromEulerAngles((float)Math.PI / 6.0f, -1 * (float)Math.PI, 0);
+    var r = f.LocalRotation.ToEulerAngles();
+    var texture = Texture.Load(@"tr_leet\texture\leet_hand.tga");
+    var matrial = new Material() { texture};
+    matrial.Shader = Shader.Default;
+    matrial.Shader.Use();
+    matrial.Shader.SetInt("texture1", 0);
+    var mesh = new Mesh(
+        new List<Vertex>{ 
+            new Vertex {Position = new Vector3(0.5f,0.5f,0.0F), TexCoords = new Vector2(1.0f,1.0f) },
+            new Vertex {Position = new Vector3(0.5f,-0.5f,0.0F), TexCoords = new Vector2(1.0f,0.0f) },
+            new Vertex {Position = new Vector3(-0.5f,-0.5f,0.0F), TexCoords = new Vector2(0.0f,0.0f) },
+            new Vertex {Position = new Vector3(-0.5f,0.5f,0.0F), TexCoords = new Vector2(0.0f,1.0f) }
+        },
+        new List<int>{ 
+            0, 1, 3, 1, 2, 3
+        }, matrial);
+    var obj = new GameObject { Parent = Scene.Current.Root };
+    mesh.Owner = obj;
 };
 
 window.Run();
