2e94785b3773d29dc1c9efbb28dc95f5e31d4a5c
cessun <cessun@tencent.com>
2023-03-31 21:55:41 +0800

    skybox

---
+++ Engine/Engine/Core/Components/SkyboxComponent.cs
@@ -0,0 +1,47 @@
+﻿using Spark.Engine.Core.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spark.Engine.Core.Components;
+
+public class SkyboxComponent : PrimitiveComponent
+{
+    public SkyboxComponent(Actor actor) : base(actor)
+    {
+
+    }
+
+    void InitRender()
+    {
+        int[] Vertex =
+        {
+            // x, y, z
+            -1, 1, -1,
+            -1, -1, -1,
+            1, -1, -1,
+            1, 1, -1,
+
+
+            -1, 1, 1,
+            -1, -1, 1,
+            1, -1, 1,
+            1, 1, 1,
+
+        };
+    }
+
+    public override void Render(double DeltaTime)
+    {
+        base.Render(DeltaTime);
+    }
+
+
+    public void RenderSkybox(double DeltaTime)
+    {
+
+    }
+
+}
