80b4a4f07d2bf06a38c9f845f21d45721882c1d8
CeSun <ce.sun@outlook.com>
2023-06-25 19:30:50 +0800

    事件映射

--- Source/Engine/Engine/GUI/NoesisGUI.cs
+++ Source/Engine/Engine/GUI/NoesisGUI.cs
@@ -1,4 +1,5 @@
-﻿using Silk.NET.Input;
+﻿using Noesis;
+using Silk.NET.Input;
 using Silk.NET.OpenGL;
 using System;
 using System.Collections.Generic;
@@ -25,9 +26,21 @@ public class NoesisGUI
         });
 
         // Noesis initialization. This must be the first step before using any NoesisGUI functionality
-        Noesis.GUI.SetLicense("Ce Sun", "JpZpg03aaYMzQP6TCJturoZUtpPys56lIS3O3DKAJlhYkJ/l");
+        //Noesis.GUI.SetLicense("Ce Sun", "JpZpg03aaYMzQP6TCJturoZUtpPys56lIS3O3DKAJlhYkJ/l");
         Noesis.GUI.Init();
+        MainKeyBoard.KeyChar += (kb, word) =>
+        {
+            OnKeyChar(word);
+        };
+        MainKeyBoard.KeyDown +=  (kb, key, num) =>
+        {
+            OnKeyDown(key);
+        };
 
+        MainKeyBoard.KeyUp += (kb, key, num) =>
+        {
+            OnKeyUp(key);
+        };
         // Setup theme
         NoesisApp.Application.SetThemeProviders();
         Noesis.GUI.LoadApplicationResources("/Noesis.GUI.Extensions;component/Theme/NoesisTheme.DarkBlue.xaml");
@@ -36,18 +49,11 @@ public class NoesisGUI
         MainMouse.MouseUp += (m, button) => OnMouseButtonUp(button, (int)m.Position.X, (int)m.Position.Y);
         Noesis.Grid xaml = (Noesis.Grid)Noesis.GUI.ParseXaml(@"
                 <Grid xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">
-                    
                     <Viewbox>
                         <StackPanel Margin=""50"">
+                            <TextBox></TextBox>
                             <Button Content=""Hello World!"" Margin=""0,30,0,0""/>
-                            <Rectangle Height=""5"" Margin=""-10,20,-10,0"">
-                                <Rectangle.Fill>
-                                    <RadialGradientBrush>
-                                        <GradientStop Offset=""0"" Color=""#40000000""/>
-                                        <GradientStop Offset=""1"" Color=""#00000000""/>
-                                    </RadialGradientBrush>
-                                </Rectangle.Fill>
-                            </Rectangle>
+                         
                         </StackPanel>
                     </Viewbox>
                 </Grid>");
@@ -85,15 +91,85 @@ public class NoesisGUI
         _view.MouseMove(x, y);
     }
 
-    public void OnMouseButtonDown(MouseButton button,int x, int y)
+    public void OnMouseButtonDown(Silk.NET.Input.MouseButton button,int x, int y)
     {
-        if (button == MouseButton.Left)
+        if (button == Silk.NET.Input.MouseButton.Left)
             _view.MouseButtonDown(x, y, Noesis.MouseButton.Left);
     }
-    public void OnMouseButtonUp(MouseButton button, int x, int y)
+    public void OnMouseButtonUp(Silk.NET.Input.MouseButton button, int x, int y)
     {
-        if (button == MouseButton.Left)
+        if (button == Silk.NET.Input.MouseButton.Left)
             _view.MouseButtonUp(x, y, Noesis.MouseButton.Left);
+    }
+
+    void OnKeyDown(Silk.NET.Input.Key key)
+    {
+        _view.KeyDown(GetUIKey(key));
+    }
+    void OnKeyUp(Silk.NET.Input.Key key)
+    {
+        _view.KeyUp(GetUIKey(key));
+    }
+    Noesis.Key GetUIKey(Silk.NET.Input.Key key)
+    {
+        return key switch
+        {
+            Silk.NET.Input.Key.Space => Noesis.Key.Space,
+            Silk.NET.Input.Key.Comma => Noesis.Key.OemComma,
+            Silk.NET.Input.Key.Backspace => Noesis.Key.Back,
+            Silk.NET.Input.Key.Up => Noesis.Key.Up,
+            Silk.NET.Input.Key.Left => Noesis.Key.Left,
+            Silk.NET.Input.Key.Right => Noesis.Key.Right,
+            Silk.NET.Input.Key.Down => Noesis.Key.Down,
+            Silk.NET.Input.Key.Enter => Noesis.Key.Enter,
+            Silk.NET.Input.Key.Escape => Noesis.Key.Escape,
+            Silk.NET.Input.Key.ControlLeft => Noesis.Key.LeftCtrl,
+            Silk.NET.Input.Key.ControlRight => Noesis.Key.RightCtrl,
+            Silk.NET.Input.Key.AltLeft => Noesis.Key.LeftAlt,
+            Silk.NET.Input.Key.AltRight => Noesis.Key.RightAlt,
+            Silk.NET.Input.Key.ShiftLeft => Noesis.Key.LeftShift,
+            Silk.NET.Input.Key.ShiftRight => Noesis.Key.RightShift,
+            Silk.NET.Input.Key.Q => Noesis.Key.Q,
+            Silk.NET.Input.Key.W => Noesis.Key.W,
+            Silk.NET.Input.Key.E => Noesis.Key.E,
+
+
+            Silk.NET.Input.Key.R => Noesis.Key.R,
+            Silk.NET.Input.Key.T => Noesis.Key.T,
+            Silk.NET.Input.Key.Y => Noesis.Key.Y,
+
+
+            Silk.NET.Input.Key.U => Noesis.Key.U,
+            Silk.NET.Input.Key.I => Noesis.Key.I,
+            Silk.NET.Input.Key.O => Noesis.Key.O,
+            Silk.NET.Input.Key.P => Noesis.Key.P,
+
+
+
+            Silk.NET.Input.Key.A => Noesis.Key.A,
+            Silk.NET.Input.Key.S => Noesis.Key.S,
+            Silk.NET.Input.Key.D => Noesis.Key.D,
+
+
+            Silk.NET.Input.Key.F => Noesis.Key.F,
+            Silk.NET.Input.Key.G => Noesis.Key.G,
+            Silk.NET.Input.Key.H => Noesis.Key.H,
+
+
+            Silk.NET.Input.Key.J => Noesis.Key.J,
+            Silk.NET.Input.Key.K => Noesis.Key.K,
+            Silk.NET.Input.Key.L => Noesis.Key.L,
+
+
+
+
+            _ => Noesis.Key.None
+        };
+
+    }
+    public void OnKeyChar(char word)
+    {
+        _view.Char(word);
     }
     int Blend;
     int BlendDstAlpha;
