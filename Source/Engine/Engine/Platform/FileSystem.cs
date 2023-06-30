ca5bf77a0e97a64a0a4420b88153e6d7f69ffeb9
CeSun <ce.sun@outlook.com>
2023-06-30 22:17:55 +0800

    S

--- Source/Engine/Engine/Platform/FileSystem.cs
+++ Source/Engine/Engine/Platform/FileSystem.cs
@@ -8,6 +8,22 @@ namespace Spark.Engine.Platform;
 
 public interface FileSystem
 {
+    public static FileSystem Instance 
+    {
+        get
+        {
+            if (_Intance == null)
+                throw new Exception("FileSystem还没有初始化");
+            return _Intance;
+        }
+    }
+
+    private static FileSystem? _Intance;
+
+    public static void Init(FileSystem fileSystem)
+    {
+        _Intance = fileSystem;
+    }
     string LoadText(string path);
 
     StreamReader GetStreamReader(string path);
