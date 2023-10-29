528198cf6e9b3575410fef7b5de813b392318fd0
CeSun <ce.sun@outlook.com>
2023-10-29 20:22:25 +0800

    增加定向光源actor

---
+++ Source/Engine/Engine/Actors/DirectionLightActor.cs
@@ -0,0 +1,32 @@
+﻿using Spark.Engine.Attributes;
+using Spark.Engine.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spark.Engine.Actors;
+
+public class DirectionLightActor : Actor
+{
+    public DirectionLightComponent DirectionLightComponent { get; private set; }
+    public DirectionLightActor(Level level, string Name = "") : base(level, Name)
+    {
+        DirectionLightComponent = new DirectionLightComponent(this);
+    }
+
+    [Property]
+    public float LightStrength
+    {
+        get => DirectionLightComponent.LightStrength;
+        set => DirectionLightComponent.LightStrength = value;
+    }
+
+    public Color Color
+    {
+        get => DirectionLightComponent.Color;
+        set => DirectionLightComponent.Color = value;
+    }
+}
