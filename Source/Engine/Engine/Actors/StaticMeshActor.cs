10ffbe93edd457d9b439b1146c3389f9380e42d1
CeSun <ce.sun@outlook.com>
2023-10-30 00:10:23 +0800

    staticmesh actor

---
+++ Source/Engine/Engine/Actors/StaticMeshActor.cs
@@ -0,0 +1,30 @@
+﻿using Spark.Engine.Assets;
+using Spark.Engine.Attributes;
+using Spark.Engine.Components;
+
+namespace Spark.Engine.Actors;
+
+public class StaticMeshActor : Actor
+{
+    [Property]
+    public StaticMeshComponent StaticMeshComponent { get; private set; }
+    public StaticMeshActor(Level level, string Name = "") : base(level, Name)
+    {
+        StaticMeshComponent = new StaticMeshComponent(this);
+    }
+
+    [Property]
+    public bool IsStatic
+    {
+        get => StaticMeshComponent.IsStatic;
+        set => StaticMeshComponent.IsStatic = value;
+    }
+
+    [Property]
+    public StaticMesh? StaticMesh
+    {
+        get => StaticMeshComponent.StaticMesh;
+        set => StaticMeshComponent.StaticMesh = value;
+    }
+
+}
