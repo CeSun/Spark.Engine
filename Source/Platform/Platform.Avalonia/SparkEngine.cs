c8807b23304244bc8266a90a1b96373ba292d980
CeSun <ce.sun@outlook.com>
2024-09-18 20:34:56 +0800

    增加ava

---
+++ Source/Platform/Platform.Avalonia/SparkEngine.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.OpenGL;
+using Avalonia.OpenGL.Controls;
+
+namespace Platform.Avalonia;
+
+public class SparkEngine : OpenGlControlBase
+{
+    protected override void OnOpenGlRender(GlInterface gl, int fb)
+    {
+        
+    }
+}
